d445b83024e9badf507bc1edf9d17b0b44e2eae6
Durand Fabrice <oeufdure@gmail.com>
2024-03-26 17:45:08 -0400

    Use information_schema instead

--- internal/Sql/Pcx/sqlobjects.go
+++ internal/Sql/Pcx/sqlobjects.go
@@ -22,7 +22,7 @@ const (
 	   Retrieve main information from Data nodes
 	*/
 
-	Dml_get_variables = "select * from performance_schema.global_variables"
+	Dml_get_variables = "select * from information_schema.global_variables"
 	Dml_get_status    = "select * from performance_schema.global_status"
 	Dml_get_pxc_view  = "select * from performance_schema.pxc_cluster_view where UUID = '?'"
 
