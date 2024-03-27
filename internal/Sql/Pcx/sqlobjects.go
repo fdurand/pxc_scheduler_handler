e2ed1504d253793cfd6c61ee89f9482c3dd26611
Durand Fabrice <oeufdure@gmail.com>
2024-03-26 20:49:16 -0400

    Use information_schema

--- internal/Sql/Pcx/sqlobjects.go
+++ internal/Sql/Pcx/sqlobjects.go
@@ -23,7 +23,7 @@ const (
 	*/
 
 	Dml_get_variables = "select * from information_schema.global_variables"
-	Dml_get_status    = "select * from performance_schema.global_status"
+	Dml_get_status    = "select * from information_schema.global_status"
 	Dml_get_pxc_view  = "select * from performance_schema.pxc_cluster_view where UUID = '?'"
 
 	Dml_get_ssl_status = "SHOW SESSION STATUS LIKE 'Ssl_cipher'"
